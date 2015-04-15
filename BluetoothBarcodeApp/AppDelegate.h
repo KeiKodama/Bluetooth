c74e975ce861d5024746f6654fcbb5b4221f0863
kodamakei <kodamakei@kodama-no-MBP.lan>
2015-04-15 10:41:02 +0900

    Initial Commit

---
+++ BluetoothBarcodeApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BluetoothBarcodeApp
+//
+//  Created by kodamakei on 2015/04/15.
+//  Copyright (c) 2015年 kccs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
